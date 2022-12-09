a8837033038a54400836a107a3d631eeb655a55e
wahidari <wahiid.ari@gmail.com>
2022-12-10 01:01:51 +0700

    update

--- components/CodeGroup.js
+++ components/CodeGroup.js
@@ -1,25 +1,23 @@
 import { Tab } from "@headlessui/react";
+import clsx from "clsx";
 import { AnimateSharedLayout, motion } from "framer-motion";
-import cn from "clsx";
 
 // https://github.com/mattrothenberg/tailwind-fancy-tab
-export default function CodeGroup({ items, children, accentColor = "border-red-500" }) {
+export default function CodeGroup({ items, children }) {
   return (
     <Tab.Group as="div" className="req-res my-4">
-      <Tab.List className="flex items-center relative bg-blue-100/60 dark:bg-gray-700/75 rounded-t-xl " >
+      <Tab.List className="flex items-center relative bg-blue-100/70 dark:bg-gray-700/75 rounded-t-xl" >
         <AnimateSharedLayout>
           {items.map((item, index) => (
             <Tab key={index + 1} as="div" className="group rounded">
               {({ selected }) => (
-                <button className={cn("relative px-4 py-3 text-neutral-700 dark:text-neutral-200 text-[13px] font-medium",
-                  `${selected ? '' : ""}`
+                <button className={clsx("relative px-4 py-2 text-[13px] font-medium transition-all duration-200",
+                  selected ? "text-neutral-800 dark:text-neutral-100" : "text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100",
                 )}>
                   {item}
                   {selected && (
                     <motion.div
-                      className={cn('absolute left-0 right-0 z-10 rounded-full h-[2px] bottom-0 border-b-2',
-                        `${accentColor ? accentColor : "border-b-blue-500"}`
-                      )}
+                      className="absolute left-0 right-0 z-10 rounded-full h-[2px] bottom-0 border-b-2 border-b-sky-500"
                       layoutId="underline"
                       initial={false}
                     />
